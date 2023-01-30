d13e9d84abc26cfe009aead44d088204e1766be2
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-30 21:15:42 +0100

    Unittests and Integration Tests
    Mock Patching

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,9 +3,29 @@
 Utils Test Module
 """
 
-import unittest
-import utils
 from parameterized import parameterized
+import unittest
+from unittest import mock
+import utils
+
+# This method will be used by the mock to replace requests.get
+
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == 'http://example.com':
+        return MockResponse({"payload": True}, 200)
+    elif args[0] == 'http://holberton.io':
+        return MockResponse({"payload": False}, 200)
+
+    return MockResponse(None, 404)
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +50,22 @@ class TestAccessNestedMap(unittest.TestCase):
             utils.access_nested_map(map, path)
 
         self.assertEqual(str(cm.exception), mess)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    A class for testing the get json of utils
+    """
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_get_json(self, mock_get):
+        json_data = utils.get_json("http://example.com")
+        self.assertEqual(json_data, {"payload": True})
+        self.assertEqual(len(mock_get.call_args_list), 1)
+
+        json_data = utils.get_json("http://holberton.io")
+        self.assertEqual(json_data, {"payload": False})
+        self.assertEqual(len(mock_get.call_args_list), 2)
 
 
 if __name__ == '__main__':
