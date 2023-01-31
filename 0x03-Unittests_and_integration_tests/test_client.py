a8fcd2b4cb039671146d20a8d6e2296f2ddf8ef1
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-31 01:53:43 +0100

    Unittests and Integration Tests
    public repos

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,3 +35,20 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_off.return_value = {"org": "abc"}
             test = GithubOrgClient("abc")
             self.assertEqual(test._public_repos_url, mock_off.return_value)
+
+    @parameterized.expand([
+        ("google", [{"name": "google"}], ["google"]),
+        ("abc", [{"name": "abc"}], ["abc"])
+    ])
+    @patch("client.get_json")
+    def test_public_repos(self, org, payload, expected, mock_json):
+        mock_json.return_value = payload
+        with patch.object(
+            GithubOrgClient, '_public_repos_url', new_callable=PropertyMock
+        ) as mock_off:
+            mock_off.return_value = "https://api.github.com/orgs/{}".format(
+                org)
+            test = GithubOrgClient(org)
+            self.assertEqual(test.public_repos(), expected)
+            mock_json.assert_called_once()
+            mock_off.assert_called_once()
