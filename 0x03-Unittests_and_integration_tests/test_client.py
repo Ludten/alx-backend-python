0395d9b4b8ecbde3dfcc00db67046d2569e570a3
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-01-31 04:47:09 +0100

    Unittests and Integration Tests
    third check

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -74,14 +74,8 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(test.has_license(lic, key), expected)
 
 
-@parameterized_class([
-    {
-        'org_payload': TEST_PAYLOAD[0][0],
-        'repos_payload': TEST_PAYLOAD[0][1],
-        'expected_repos': TEST_PAYLOAD[0][2],
-        'apache2_repos': TEST_PAYLOAD[0][3],
-    },
-])
+@parameterized_class(('org_payload', 'repos_payload',
+                      'expected_repos', 'apache2_repos'), TEST_PAYLOAD)
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Performs integration tests for the `GithubOrgClient` class."""
     @classmethod
